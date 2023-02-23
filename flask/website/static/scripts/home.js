5fcf93aa9f58586c3569dffd6a460b9052fcb229
Zeyrux <tappetheo@gmail.com>
2023-02-23 11:07:39 +0100

    improved home background image

--- flask/website/static/scripts/home.js
+++ flask/website/static/scripts/home.js
@@ -1,12 +1,20 @@
-function init() {
+function handle_resize() {
   let img = document.getElementById("background_image");
   let ratio_img = img.offsetWidth / img.offsetHeight;
   let ratio_window = window.innerWidth / window.innerHeight;
   if (ratio_img > ratio_window) {
     img.height = window.innerHeight;
+    img.style.width = "auto";
+    img.style.width.removeProperty("height");
   } else {
     img.width = window.innerWidth;
+    img.style.height = "auto";
+    img.style.removeProperty("width");
   }
 }
 
-// init();
+function init() {
+  window.addEventListener("resize", handle_resize);
+}
+
+init();
